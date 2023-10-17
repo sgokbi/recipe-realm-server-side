6fd4aadaf7a294b6ced94931d303c963a9f1d28d
Sgokbi <sagorika.tech@gmail.com>
2023-10-18 01:21:04 +0300

    chefs/:id done

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const port = process.env.PORT || 5000;
 
+app.use(cors());
 const chefs = require("./data/chefs.json");
 
 app.get("/", (req, res) => {
@@ -10,6 +12,13 @@ app.get("/", (req, res) => {
 
 app.get("/chefs", (req, res) => {
   res.send(chefs);
+});
+
+app.get("/chefs/:id", (req, res) => {
+  const id = req.params.id;
+  console.log(id);
+  const selectedChef = chefs.find((chef) => chef.id === id);
+  res.send(selectedChef);
 });
 
 app.listen(port, () => {
